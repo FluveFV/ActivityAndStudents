f23c3b47290aba254fd1897cf7a1fb42909cdce0
majada14 <137620909+majada14@users.noreply.github.com>
2023-11-16 16:17:55 +0100

    Update general.py

--- general.py
+++ general.py
@@ -1,50 +1,49 @@
 import pandas as pd
 import datetime
+import numpy as np
+from datetime import timedelta
 import tqdm
 
 # load the datasets
 inpath1 = "../dati/td_ita.dta" # insert system path for time diary !
 inpath2 = "../dati/data4diarynew_ITA.dta" # insert system path for demographics! !
-inpath3 = "../dati/meteo_nov_2020.csv" # insert system path for weather !
 
 td_dataset = pd.read_stata(inpath1)  # time diaries dataset
 demo_dataset = pd.read_stata(inpath2) # demographics dataset
 
-""" Cleaning procedure of the time diaries dataset """
+""" --- Cleaning procedure of the time diaries dataset --- """
 """ we need to apply various steps:
 1. consider only first two weeks of the survey
 2. consider only the variables we want to investigate
-3. consider only the physical activities"""
+3. consider only the sport activities"""
 
 # cleaning 1
 td_cleaned = td_dataset[td_dataset['first2w'] == 'First two weeks']
 
 # cleaning 2
-td_cleaned = td_cleaned[['id', 'date_not','what', 'withw', 'where', 'sport']]
+td_cleaned = td_cleaned[['id', 'date_not', 'what', 'withw', 'where', 'sport']]
 
 # cleaning 3
 td_cleaned= td_cleaned[(td_cleaned["what"]=="Sport") | (td_cleaned["what"]=="I will participate in sports activities")]
 
-print(f"From the first cleaning process we obtain the dataset: \n {td_cleaned}")
 
 """ Variable transformation """
 """
 we want to add new variables:
-- weather: the categories are 'level of precipitation'
 - time session: duration of the sport activity
 
 we want to apply a reclassification of the following variables:
 - where column: the new categories are 'indoor - home', 'indoor - gym', 'outdoor'
 - with who: the new categories are 'company', 'alone'
-- time slot: the new categories are 'morning', 'lunch', 'afternoon', 'dinner', 'night'
+- time slot: the new categories are 'morning', 'midday', 'afternoon', 'evening', 'night'
 """
 
 # time session variable
 new_td = td_cleaned
 new_td['date_not'] = pd.to_datetime(new_td['date_not'])
-new_td['date_dur'] = new_td['date_not'] - timedelta(hours = 5) # in this way we can consider sport sessions across two days as single sport sessions
+new_td['date_dur'] = new_td['date_not'] - timedelta(hours = 5)
 
-new_td = new_td .sort_values('date_dur') # sort the dataframe according to "date_not"
+new_td = new_td .sort_values('date_dur') # sort the dataframe according to "date_dur"
 new_td['time_diff'] = new_td['date_dur'].diff() # compute the time difference between adjacent rows
 
 new_session = (new_td['time_diff'] > pd.Timedelta(minutes=30)) | new_td['time_diff'].isnull() # search for new start session
@@ -53,128 +52,132 @@ new_td['session'] = new_session.cumsum() # Creazione di un nuovo identificatore 
 collapsed_sessions = new_td.groupby(['id', 'session']).agg(
     start_time=('date_not', 'first'),
     end_time=('date_not', 'last'),
-    duration=('date_not', lambda x: ((x.max() - x.min()).total_seconds() / 60)+30)
+    duration=('date_dur', lambda x: ((x.max() - x.min()).total_seconds() / 60)+30)
 ).reset_index()
 
 collapsed_sessions = collapsed_sessions.drop(columns='session')
 
 # merge the two datasets
 td_cleaned = td_cleaned.merge(collapsed_sessions, left_on=['id', 'date_not'], right_on=['id', 'start_time']) # inner join of the two datasets
-print(f"From the first transformation process we obtain the dataset: \n {td_cleaned}")
 
 # where variable
-indoor_home = ['Home apartment /room', 'Weekend home or holiday apartment', 'House (friends others)', 'Relatives Home']
-indoor_gym = ['Another indoor place', 'Gym, swimming pool, Sports centre …', 'Other university place']
-outdoor = ['Countryside/mountain/hill/beach', 'Home garden/patio/courtyard', 'In the street', 'Another outdoor place',
-           'Café, pub, bar', 'Shops, shopping centres']
-
-# function for reclassification
-def where_reclass(row):
-    if row['where'] in indoor_dome:
-        new_class = 'indoor - home'
-    elif row['where'] in outdoor:
-        new_class = 'outdoor'
-    elif row['where'] in indoor_gym:
-        new_class = 'indoor - gym'
-    else:
-        new_class = np.nan
-
-    return new_class
-
-td_cleaned['new where'] = td_cleaned.apply(where_reclass, axis=1)
+td_cleaned['where recoded'] = td_cleaned['where'].replace(['Home apartment /room', 'Weekend home or holiday apartment', 'House (friends others)', 'Relatives Home', 'Home garden/patio/courtyard',
+                                                           'Another indoor place', 'Gym, swimming pool, Sports centre …', 'Other university place', 'Countryside/mountain/hill/beach', 'In the street', 'Another outdoor place', 'Café, pub, bar', 'Shops, shopping centres',
+                                                           'Not answer'],
+                                                          ['indoor', 'indoor', 'indoor', 'indoor', 'indoor',
+                                                           'outdoor', 'outdoor', 'outdoor', 'outdoor', 'outdoor', 'outdoor', 'outdoor', 'outdoor',
+                                                           'Not answer where'])
 
 # with who variable
-company = ['Partner', 'Friend(s)', 'Relative(s)', 'Roommate(s)', 'Other', 'Colleague(s)', 'Classmate(s)']
+td_cleaned['withw recoded'] = td_cleaned['withw'].replace(['Partner', 'Friend(s)', 'Relative(s)', 'Roommate(s)', 'Other', 'Colleague(s)', 'Classmate(s)',
+                                                           'Alone',
+                                                           'Not answer'],
+                                                          ['company', 'company', 'company', 'company', 'company', 'company', 'company',
+                                                           'alone',
+                                                           'Not answer withw'])
 
-# function for reclassification
-def with_reclass(row):
-    if row["withw"] in company:
-        new_class = "company"
-    else:
-        new_class = "alone"
+# time variable
+td_cleaned['hours'] = td_cleaned['date_not'].dt.hour
 
-    return new_class
+# sport variable
+td_cleaned['sport recoded'] = td_cleaned['sport'].replace(['Walking, Trekking, and hiking',
+                                                           'Jogging and running',
+                                                           'Cycling, skiing, and skating', 'Ball games', 'Other outdoor activities',
+                                                           'Gymnastics and fitness',
+                                                           'Water sports', 'Other indoor activities'],
+                                                          ['Walking, Trekking, and hiking',
+                                                           'Jogging and running',
+                                                           'Outdoor activities', 'Outdoor activities', 'Outdoor activities',
+                                                           'Gymnastics and fitness',
+                                                           'Other indoor activities', 'Other indoor activities'])
 
-td_cleaned['new withw'] = td_cleaned.apply(with_reclass, axis=1)
-print(f"From the second transformation process we obtain the dataset: \n {td_cleaned}")
-
-# date variable reclass
-def date_reclass(row):
-    elements = str(row["date_not"])[:10].split("-")
-    year = int(elements[0])
-    month = int(elements[1])
-    day = int(elements[2])
-    new_date = datetime.date(year, month, day)
-
-    return new_date
-
-td_cleaned['date'] = td_cleaned.apply(date_reclass, axis=1)
-
-# date variable reclass
-def time_reclass(row):
-    cleaning_1 = str(row["date_not"])
-    hours = int(cleaning_1[11:13])
-    minutes = int(cleaning_1[14:16])
-    new_time = datetime.time(hours, minutes, 0)
-
-    return new_time
-
-td_cleaned['time'] = td_cleaned.apply(time_reclass, axis=1)
-print(f"From the third transformation process we obtain the dataset: \n {td_cleaned}")
 
 # function for reclassification of the hours
 def timeslot_reclass(row):
-    if row['time'] >= datetime.time(5, 0, 0) and row["time"] < datetime.time(12, 0, 0): 
+    if row["hours"] < 12:
         new_class = "morning"
-    elif row["time"] >= datetime.time(12, 0, 0) and row["time"] < datetime.time(14, 0, 0):
+    elif row["hours"] >= 12 and row["hours"] < 14:
         new_class = "midday"
-    elif row["time"] >= datetime.time(14, 0, 0) and row["time"] < datetime.time(19, 0, 0):
+    elif row["hours"] >= 14 and row["hours"] < 19:
         new_class = "afternoon"
-    elif row["time"] >= datetime.time(19, 0, 0) and row["time"] < datetime.time(21, 0, 0):
+    elif row["hours"] >= 19 and row["hours"] < 21:
         new_class = "evening"
     else:
         new_class = "night"
     return new_class
 
-td_cleaned['new time'] = td_cleaned.apply(timeslot_reclass, axis=1)
-print(f"From the fourth transformation process we obtain the dataset: \n {td_cleaned}")
+td_cleaned['hours recoded'] = td_cleaned.apply(timeslot_reclass, axis=1)
 
-# weather variable
-nov_meteo = pd.read_csv(inpath3)
-print(f"weather for the month of all the days of november 2020: \n {nov_meteo.head()}")
-
-# reclass of day variable (in order to merge with td_cleaned dataset)
-vals = list(nov_meteo["date"].values)
-new_vals = []
-
-for el in vals:
-    current_el = el.split(" ")
-    day = int(current_el[1])
-    new_date = datetime.date(2020, 11, day)
-    new_vals.append(new_date)
-
-# new meteo dataset
-meteo_data = {'date': pd.Series(new_vals),
-              'prec': nov_meteo['prec'],
-              'tmin': nov_meteo['tmin'],
-              'tmax': nov_meteo['tmax']}
-
-new_nov_meteo = pd.DataFrame(data=meteo_data)
-print(f"weather dataset cleaned is now: \n {new_nov_meteo}")
-
-# converion of the userid into an integer
+# conversion of the userid into an integer
 td_cleaned['id'] = td_cleaned['id'].astype(int)
 demo_dataset['userid'] = demo_dataset['userid'].astype(int)
 
-# we save a dataset for descritpive analysis
-td_cleaned.to_csv('descriptive_dataset.csv', index=False)
+# consider only the variables we want to investigate
+td_cleaned = td_cleaned[['id', 'sport', 'duration', 'where recoded', 'withw recoded', 'hours recoded','sport recoded']]
+total_dur = td_cleaned[['id','duration']].groupby(['id'], as_index=False).sum()
 
-# merge the two datasets
-td_cleaned = td_cleaned[['id', 'duration', 'new where', 'new withw', 'date', 'new time']]
-td_cleaned = td_cleaned.merge(new_nov_meteo, left_on='date', right_on='date', how='left') # left join of the two datasets
+# where variable
+where_df = td_cleaned.pivot(columns=['where recoded'], values='duration')
+where_df = where_df.fillna(0)
+where_df = where_df[['indoor', 'outdoor', 'Not answer where']]
+
+# with who variable
+withw_df = td_cleaned.pivot(columns=['withw recoded'], values='duration')
+withw_df = withw_df.fillna(0)
+withw_df = withw_df[['alone', 'company', 'Not answer withw']]
+
+# time variable
+time_df = td_cleaned.pivot(columns=['hours recoded'], values='duration')
+time_df = time_df.fillna(0)
+time_df = time_df[['morning', 'midday', 'afternoon', 'evening', 'night']]
+
+# sport variable
+sport_df = td_cleaned.pivot(columns=['sport recoded'], values='duration')
+sport_df = sport_df.fillna(0)
+sport_df = sport_df[['Walking, Trekking, and hiking', 'Jogging and running', 'Outdoor activities', 'Gymnastics and fitness', 'Other indoor activities']]
+
+# merge all the dataframes together
+final_data = {'id': pd.Series(td_cleaned['id']),
+              'indoor': pd.Series(where_df['indoor']),
+              'outdoor': pd.Series(where_df['outdoor']),
+              'alone': pd.Series(withw_df['alone']),
+              'company': pd.Series(withw_df['company']),
+              'morning': pd.Series(time_df['morning']),
+              'midday': pd.Series(time_df['midday']),
+              'afternoon': pd.Series(time_df['afternoon']),
+              'evening': pd.Series(time_df['evening']),
+              'night': pd.Series(time_df['night']),
+              'walking': pd.Series(sport_df['Walking, Trekking, and hiking']),
+              'running': pd.Series(sport_df['Jogging and running']),
+              'outdoor activities': pd.Series(sport_df['Outdoor activities']),
+              'fitness': pd.Series(sport_df['Gymnastics and fitness']),
+              'indoor activities': pd.Series(sport_df['Other indoor activities']),
+              }
+
+final_dataset = pd.DataFrame(data=final_data)
+
+final_dataset = final_dataset.groupby('id', as_index=False).sum()
 
 # merge the td_cleaned dataset with the demographic dataset
-final_dataset = td_cleaned.merge(demo_dataset, left_on='id', right_on='userid', how='left')
+almost_dataset = final_dataset.merge(demo_dataset, left_on='id', right_on='userid', how='left')
+complete_dataset = almost_dataset.merge(total_dur, left_on='id', right_on='id', how='left')
 
-print(f"the final dataset with sport sessions and demographic features is: \n {sport_demo}")
-final_demo.to_csv('sport_dataset.csv', index=False)
+cat_dataset = td_cleaned.merge(demo_dataset, left_on='id', right_on='userid', how='left')
+
+# clean the dataset by keeping only the variables we want to investigate
+complete_dataset = complete_dataset[['id', 'duration', 'indoor', 'outdoor', 'alone', 'company', 'morning', 'midday',
+                                     'afternoon', 'evening', 'night','walking',
+                                     'running', 'outdoor activities', 'fitness', 'indoor activities',
+                                     'degree', 'department', 'w1_A01']]
+
+# reclassification of department variable
+
+'''
+complete_dataset['department recoded'] = complete_dataset['department'].replace(['Engineering and Applied Sciences', 'Natural Sciences', 'Medicine and veterinary medicine', 'Agricultural',
+                                                                                 'Business/economics', 'Humanities', 'Law', 'International Relations and Public Administration', 'Social Sciences'],
+                                                                                ['STEM', 'STEM', 'STEM', 'STEM',
+                                                                                 'humanities', 'humanities', 'humanities', 'humanities', 'humanities'])
+'''
+
+cat_dataset.to_csv('data/categorical_sport_dataset.csv', index=False)
+complete_dataset.to_csv('data/continuous_sport_dataset.csv', index=False)
